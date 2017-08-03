563a4dbbcd85530594c4c6e1ae7c5bc9c1fd4d09
Jenny <jennyngo.dev@gmail.com>
2017-08-03 13:33:41 -0400

    added images, add handlebars

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,1 +1,9 @@
 console.log('helloo world')
+var myTemplate = $('#welcome').html();
+var template = Handlebars.compile(myTemplate);
+var person = {
+    name: 'Joe Smith'
+};
+
+var personTemplate = template(person);
+$('#prompt').append(personTemplate);
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
 console.log('helloo world');
+var myTemplate = $('#welcome').html();
+var template = Handlebars.compile(myTemplate);
+var person = {
+    name: 'Joe Smith'
+};
+
+var personTemplate = template(person);
+$('#prompt').append(personTemplate);
